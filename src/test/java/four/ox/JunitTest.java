3b323817d3333592fb19e63d4f4f3e512698b606
4ox <mail@4ox.kr>
2020-03-31 21:43:35 +0900

    junit test

---
+++ src/test/java/four/ox/JunitTest.java
@@ -0,0 +1,14 @@
+package four.ox;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class JunitTest {
+	@Test
+	public void test() {
+		int a = 1;
+		int b = 2;
+		assertEquals(a *2, b);
+	}
+}
